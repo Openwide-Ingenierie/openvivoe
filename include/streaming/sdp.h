b48ac53d1d992ba3f0d0c16e7f774a32f29e4dee
hoel <hoel.vasseur@openwide.fr>
2016-02-26 10:54:37 +0100

    Create sdp.c and sdp.h files
    Those files will be used to create the SDP messages to send to the
    Service User, over SAP protocol. Those SDP messages contain information
    used to describe and decode the stream.

---
+++ include/streaming/sdp.h
@@ -0,0 +1,13 @@
+/*
+ * Licence: GPL
+ * Created: Fri, 26 Feb 2016 10:53:53 +0100
+ * Main authors:
+ *     - hoel <hoel.vasseur@openwide.fr>
+ */
+#ifndef SDP_H
+# define SDP_H
+
+
+
+#endif /* SDP_H */
+
---
+++ src/streaming/sdp.c
@@ -0,0 +1,10 @@
+/*
+ * Licence: GPL
+ * Created: Fri, 26 Feb 2016 10:53:40 +0100
+ * Main authors:
+ *     - hoel <hoel.vasseur@openwide.fr>
+ */
+
+
+
+
