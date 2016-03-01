69ef6cb22c22ad2007902a26fecbc982fbb07db1
hoel <hoel.vasseur@openwide.fr>
2016-03-01 17:05:06 +0100

    Add the differenciation between RAW and MPEG-4 in sdp.c
    Before, sdp.c and espacially createSDP did not take into account the
    fact that the SDP files to build are different if we use MPE-4 or RAW
    (or J2K) videos. Indeed, the SDP files differs in media field (in the
    ftmp attribute): raw gives several different fields (for example width,
    height) however, mpeg-4 gives only two parameters: config and
    profile-level-id. This is now taken into account.

--- src/streaming/sdp.c
+++ src/streaming/sdp.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <glib-2.0/glib.h>
 #include <gstreamer-1.0/gst/gst.h>
 #include <gstreamer-1.0/gst/sdp/gstsdpmessage.h>
@@ -46,6 +47,80 @@ static char* interlace_mode_to_string(int interlaced_mode){
 		default: return "";
 	}
 }
+
+/**
+ * \brief create Media for in RAW video style
+ * \param channel_entry an entry in the channelTable to retrieve relevant inforlation
+ * \param media the media to modify: where to store the new information
+ */
+static gboolean create_raw_media(struct channelTable_entry * channel_entry, GstSDPMedia *media)
+{
+	gchar *fmtp =  g_strdup_printf ("sampling=%s; width=%ld; height=%ld; depth=%ld; colorimetry=%s; %s",
+										channel_entry->channelVideoSampling,
+										channel_entry->channelHorzRes,
+										channel_entry->channelVertRes, 
+										channel_entry->channelVideoBitDepth,
+										channel_entry->channelColorimetry,
+										interlace_mode_to_string (channel_entry->channelInterlaced)
+										);	
+	if( gst_sdp_media_add_attribute (media, "fmtp", fmtp)!= GST_SDP_OK ){
+		g_printerr("ERROR: problem in media creation for SDP file\n");
+		return FALSE;
+	}
+	return TRUE;
+}
+
+/**
+ * \brief create Media for in MPEG-4 video style
+ * \param channel_entry an entry in the channelTable to retrieve relevant inforlation
+ * \param media the media to modify: where to store the new information
+ */
+static gboolean create_mpeg4_media(struct channelTable_entry * channel_entry, GstSDPMedia *media)
+{
+	stream_data *data = channel_entry->stream_datas;
+	gchar  		*fmtp =  g_strdup_printf( 	"profile-level-id=%s, config=%s", 
+											data->rtp_datas->profile_level_id, 
+											data->rtp_datas->config
+										);	
+	if( gst_sdp_media_add_attribute (media, "fmtp", fmtp)!= GST_SDP_OK ){
+		g_printerr("ERROR: problem in media creation for SDP file\n");
+		return FALSE;
+	}
+	return TRUE;	
+}
+
+/**
+ * \brief an function that display an appropriate error message
+ * \return FALSE (as it is an error ;) )
+ */
+static gboolean error_function(){
+	g_printerr("ERROR:Failed SDP file\n");
+		return FALSE;
+}
+
+/**
+ * \brief create Media for in RAW video style
+ * \param channel_entry an entry in the channelTable to retrieve relevant inforlation
+ * \param media the media to modify: where to store the new information
+ */
+static gboolean create_fmtp_media(struct channelTable_entry * channel_entry, GstSDPMedia *media)
+{
+	if ( !strcmp( channel_entry->channelVideoFormat, "RAW")){ // case RAW format
+		if( !create_raw_media(channel_entry, media))
+			return error_function();
+	}else if ( !strcmp(channel_entry->channelVideoFormat , "MP4V-ES")){ // case MPEG-4 format
+		if( !create_mpeg4_media(channel_entry, media))
+			return error_function();
+	}/*else if ( !strcmp( channel_entry->channelVideoFormat , "J2000")){ // case J2000 format
+		if( !create_mpeg4_media(channel_entry, media))
+			return error_function();
+	}*/else{
+		return error_function();
+	}
+	return TRUE;
+}
+
+
 
 /** 
  * \brief Create the SDP message corresponding to the stream
@@ -131,28 +206,34 @@ gboolean create_SDP(struct channelTable_entry * channel_entry ){
 	 * m=<media> <port> <transport> <fmt list>
 	 */
 	GstSDPMedia *media;
-	gst_sdp_media_new (&media);
-	gst_sdp_media_set_media (media, media_type );
-	gst_sdp_media_set_port_info (media, DEFAULT_MULTICAST_PORT, 1 );
-	gst_sdp_media_set_proto ( media,transport_proto );
-	gst_sdp_media_add_format(media, g_strdup_printf ("%ld", data->rtp_datas->rtp_type));
+	if( gst_sdp_media_new (&media)!= GST_SDP_OK)
+		return error_function();
+	if(	gst_sdp_media_set_media (media, media_type )!= GST_SDP_OK)
+		return error_function();
+	if (gst_sdp_media_set_port_info (media, DEFAULT_MULTICAST_PORT, 1 )!= GST_SDP_OK)
+		return error_function();
+	if ( gst_sdp_media_set_proto ( media,transport_proto )!= GST_SDP_OK)
+		return error_function();
+
+	if( gst_sdp_media_add_format(media, g_strdup_printf ("%ld", data->rtp_datas->rtp_type))!= GST_SDP_OK)
+		return error_function();
+
 	/* convert rtp_type into string, max value is 107 so a 4 bytes string */
 	gchar *rtmpmap =  g_strdup_printf ("%ld %s/%d", data->rtp_datas->rtp_type,channel_entry->channelVideoFormat ,data->rtp_datas->clock_rate );
-	gst_sdp_media_add_attribute (media, "rtmpmap",rtmpmap );
-	gchar *fmtp =  g_strdup_printf ("sampling=%s; width=%ld; height=%ld; depth=%ld; colorimetry=%s; %s",
-										channel_entry->channelVideoSampling,
-										channel_entry->channelHorzRes,
-										channel_entry->channelVertRes, 
-										channel_entry->channelVideoBitDepth,
-										channel_entry->channelColorimetry,
-										interlace_mode_to_string (channel_entry->channelInterlaced)
-										);	
-	gst_sdp_media_add_attribute (media, "fmtp", fmtp);
+	if( gst_sdp_media_add_attribute (media, "rtmpmap",rtmpmap ) != GST_SDP_OK)
+		return error_function();
+
+	create_fmtp_media(channel_entry, media);
+
+
+//***/*//*/*/*/*/*/*/*/**/*/*/
+	/* Add framerate to media information */
 	gchar *framerate =  g_strdup_printf("%ld", channel_entry->channelFps);
-	gst_sdp_media_add_attribute (media, "framerate", framerate);
-	
-	/* Add the media to SDP message */
-	gst_sdp_message_add_media (msg, media);
+	if( gst_sdp_media_add_attribute (media, "framerate", framerate)!= GST_SDP_OK )	/* Add the media to SDP message */
+		return error_function();		
+	if ( gst_sdp_message_add_media (msg, media))
+		return error_function();		
+
 	printf("%s\n", gst_sdp_message_as_text(msg));
 	 return TRUE;	
 }
