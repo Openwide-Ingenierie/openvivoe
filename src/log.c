500c73fb56f263043816fb44be2a80a246434d5d
hoel <hoel.vasseur@openwide.fr>
2016-04-15 17:15:48 +0200

    log.c: replace g_printerr by g_warning
    The function g_warning is more adapted here as we don't want to exit the
    program. An error should completely stop and exit the program. However,
    with failures such as "Cannot make element foo" we do not want to exit
    the program otherwise all steams connected to this one will be stopped.

--- src/log.c
+++ src/log.c
@@ -19,7 +19,7 @@ gboolean gst_element_link_log(	GstElement *element1 ,  GstElement *element2 ){
 	/* link element1 to element2 payloader */
 	if ( !gst_element_link(element1,element2 )){
 
-		g_printerr("failed to link %s to %s\n", GST_ELEMENT_NAME(element1), GST_ELEMENT_NAME(element2));
+		g_warning("failed to link %s to %s\n", GST_ELEMENT_NAME(element1), GST_ELEMENT_NAME(element2));
 		return FALSE;
 	}
 
@@ -38,11 +38,10 @@ GstElement *gst_element_factory_make_log( const gchar *element,  const gchar *na
 	
 	/* check if everything went ok, if not, display an error message */
 	if ( !return_elt ){
-		g_error("cannot create element %s named %s\n", element , name);
+		g_warning("cannot create element %s named %s\n", element , name);
 		return NULL;
 	}
 
 	return return_elt;
 
 }
-
