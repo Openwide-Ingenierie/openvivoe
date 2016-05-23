e815310cbd082b47bc4fd1315335a126f3c1a7d6
hoel <hoel.vasseur@openwide.fr>
2016-05-23 12:00:24 +0200

    create ip_assignment.c and .h files
    This files will contain VIVOE's IP assignment scheme, as described in
    the VIVOE Standard. I already added two functions set_static_ip() and
    detect_confilct(). The first one assigned a static IP given in
    parameters to the interface also given into parameters.
    The second one will be use to detect conflict(s) just as it is described
    in the MIB.

---
+++ include/ip_assignment.h
@@ -0,0 +1,18 @@
+/*
+ * Licence: GPL
+ * Created: Mon, 23 May 2016 10:54:49 +0200
+ * Main authors:
+ *     - hoel <hoel.vasseur@openwide.fr>
+ */
+#ifndef IP_ASSIGNMENT_H
+#define IP_ASSIGNMENT_H
+
+
+/**
+ * \brief the default static IP that shoud be assgigned to a device
+ */
+#define DEAFULT_STATIC_IP 	"192.168.204.254"
+
+
+#endif /* IP_ASSIGNMENT_H */
+
