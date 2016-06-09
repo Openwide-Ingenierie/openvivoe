e351192ae7631c58e66866d0a956b354dcd4ff64
hoel <hoel.vasseur@openwide.fr>
2016-06-09 10:36:40 +0200

    multicast.h: remove commented line

--- include/multicast.h
+++ include/multicast.h
@@ -7,7 +7,6 @@
 #ifndef MULTICAST_H
 # define MULTICAST_H
 
-//long define_vivoe_multicast(const char* multicast_iface, const short int multicast_channel);
 long define_vivoe_multicast( struct ethernetIfTableEntry *if_entry, const short int multicast_channel);
 
 #endif /* MULTICAST_H */
